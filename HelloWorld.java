e9f0f6a1920ef3d038c8ccc870a15330f0fa3eef
“cc” <1774652513@qq.com>
2021-06-07 19:16:01 +0800

    daima

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+package com.lemon.helloworld;
+//Java自动化的第一个方法
+/*
+就是想记录下自己学习的注释多行方法
+ */
+
+/**
+ * OK,this is my first java.
+ */
+public class HelloWorld {
+   public static void main(String [] args ){
+      System.out.println("shuxue ");
+      System.out.println("feifei");
+      String a="feifie";
+      System.out.println(a);
+
+
+   }
+
+   }
+
